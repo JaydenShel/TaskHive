9d6b79d950c578725f3b0f796320a779f705a1dd
jaysh <jshelderfer@ufl.edu>
2025-01-08 15:37:55 -0500

    dbCreateAccount

--- backend/api/account.js
+++ backend/api/account.js
@@ -1,14 +1,18 @@
 const express = require("express")
+const queryDatabase = require("../database")
 const router = express.Router();
 
 router.post('/', (req, res) => {
-    //const {selectedUsername, selectedPassword} = req.body;
+    const {selectedUsername, selectedPassword} = req.body;
+    const created_at = new Date();
     //Query the database, create a new column with username, password
     try{
+        queryDatabase('INSERT INTO credentials (username, password, created_at) VALUES ($1, $2, $3)', [selectedUsername, selectedPassword, created_at])
         return res.status(200).json({message: "Account Creation Successful"})
     }
     //Catch any errors, store as a message in response
     catch(error){
+        console.log(error);
         return res.status(402).json({message: error});
     }
 })
