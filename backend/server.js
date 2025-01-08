c9a808ba9d43ab55a33ecc6307f6157a0472b8e6
jaysh <jshelderfer@ufl.edu>
2025-01-07 19:06:02 -0500

    dbConnection

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,38 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const { Client } = require('pg');
 
 dotenv.config();
+
+//Create client
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+//Connect to Heruko postgres database
+client.connect()
+    .then(() => {
+        console.log('Successfully connected to the Heroku Postgres database!');
+    })
+    .catch(err => {
+        console.error('Database connection error:', err.stack);
+    });
+
+//Take in any query and value for said query, send to database
+const queryDatabase = async (query, params) =>{
+    try{
+        const result = await client.query(query, params);
+        return result.rows;
+    }
+    catch(error){
+        console.error("Problem executing query:", error);
+        throw error;
+    }
+}
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -22,3 +52,5 @@ app.use('/account', accountRoute);
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
+module.exports = { queryDatabase };
